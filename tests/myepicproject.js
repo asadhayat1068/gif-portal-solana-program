4074bb370176e3106b08ccb769b2caa5ee9d195c
Asad Hayat <asadhayat.1068@gmail.com>
2022-08-25 03:29:05 +0500

    Store Basic data on contract

--- tests/myepicproject.js
+++ tests/myepicproject.js
@@ -1,13 +1,39 @@
 const anchor = require("@project-serum/anchor");
+const { SystemProgram } = anchor.web3;
 
 const main = async () => {
   console.log('Starting test...');
 
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
   const program = anchor.workspace.Myepicproject;
-  const tx = await program.rpc.startStuffOff();
+
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  const tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId
+    },
+    signers: [baseAccount],
+  });
 
   console.log('Your Tx Signature is: ', tx);
+
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('Total GIFS = ', account.totalGifs.toString());
+
+  const tx2 = await program.rpc.addGif({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+    }
+  });
+
+  console.log('Your Tx2 Signature is: ', tx2);
+
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('Total GIFS = ', account.totalGifs.toString());
 }
 
 const runMain = async () => {
